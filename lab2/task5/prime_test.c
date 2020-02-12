61c0b43cb5b692e22c40ac1b309f8088dbd47c89
Peiying Yang <py30732n@pace.edu>
2020-02-12 18:54:07 -0500

    Task5

--- lab2/task5/prime_test.c
+++ lab2/task5/prime_test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 void get_valid_number(int *n);
-bool is_prime(int x)
+bool is_prime(int x);
 
 //main() uses a sentinel controlled loop to test the primality of numbers
 //, without knowing how many numbers to be tested.
@@ -19,7 +20,7 @@ int main()
 		else {
 			printf("%d is not a prime number!\n", n);
 		}
-		int n;
+		//int n;
 		//read again then loop back to test
 		get_valid_number(&n);
 	}
@@ -28,23 +29,24 @@ int main()
 
 bool is_prime(int n) {
 	//if n is divisible by any number greater or equal to 2 and less than n, then n is not prime.
+	bool flag = true;
 	for (int i = 2; i < n; i++) {
 		if (n%i == 0) {
+			flag = false;
 			break;
 		}
 	}
-	return !(n%i==0);
+	return flag;
 
 }
 
 void get_valid_number(int * n) {
 	//The valid number is either -1 (to exit the program) or an integer greater than 2.
-	//A loop is used to implement input validation, that is, user will be asked to 
+	//A loop is used to implement input validation, that is, user will be asked to
 	//enter a valid number over and over again until a valid number is received.
-        printf("Please enter an integer greater than 2 to test its primality, enter -1 to exit \n");
-	scanf("%d", n);
-	while (*n <= 2 || *n != -1) {
+	do {
+		/* code */
 		printf("Please enter an integer greater than 2 to test its primality, enter -1 to exit \n");
 		scanf("%d", n);
+		} while (*n <= 2 && *n != -1);
 	}
-}
