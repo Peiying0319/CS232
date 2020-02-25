2a4670b012d4487629b9ba279d167bf91a87fd11
Peiying Yang <py30732n@pace.edu>
2020-02-24 23:53:09 -0500

    Task3

--- lab3/task3/wordstats.c
+++ lab3/task3/wordstats.c
@@ -6,11 +6,26 @@ int main () {
 
   /* Zero out the array */
   int letter_frequency[26] = {0};
-  int len, i;
+  int len, i, c, order;
   char buf[MAX_BUF];
 
+  printf("Enter a string to get letter frequency:\n ");
+  fgets(buf, MAX_BUF,stdin);
+  len = strlen(buf);
+
   do {
-  } while (len > 1);
+
+    for(c = 0; c < len; c++){
+      if(buf[c] >= 'a' && buf[c] <= 'z'){
+        order = buf[c] - 97;
+        letter_frequency[order]++;
+      }
+      else if(buf[c] >= 'A' && buf[c] <= 'Z'){
+      order = buf[c] - 65;
+      letter_frequency[order]++;
+      }
+    }
+  } while (c < len);
 
   printf("Distribution of letters in corpus:\n");
   for (i = 0; i < 26; i++) {
