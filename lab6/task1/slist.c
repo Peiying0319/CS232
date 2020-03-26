2b5417a06b8911f4af67e204a7b331a1340793e0
Peiying Yang <py30732n@pace.edu>
2020-03-26 19:51:55 -0400

    task1

---
+++ lab6/task1/slist.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include "slist.h"
+
+
+/**
+ * Allocates new slist dynamically.
+ *
+ * @return pointer to the list
+ */
+struct slist *slist_create()
+{
+    struct slist *s = malloc(sizeof(struct slist));
+    s->front = NULL;
+    s->back = NULL;
+    s->size = 0;
+    return s;
+}
+
+/**
+ * Inserts new node in slist after the last node.
+ *
+ * @param l pointer to the list (non-NULL)
+ * @param str pointer to a C string to store in new list node
+ * returns a pointer to the newly added node
+ */
+
+struct snode* slist_add_back(struct slist *l, char *str)
+{
+    struct snode *new_node = snode_create(str);
+    l->back->next = new_node;
+    l->back = new_node;
+    l->size = l->size + 1;
+    return new_node;
+}
+
+/**
+ * Inserts new node in slist before the first node.
+ *
+ * @param l pointer to the list (non-NULL)
+ * @param str pointer to a C string to store in new list node
+ * returns a pointer to the newly added node
+ */
+
+struct snode* slist_add_front(struct slist *l, char *str)
+{
+    struct snode *new_node = snode_create(str);
+    new_node->next = l->front;
+    l->front = new_node;
+    l->size = l->size + 1;
+    return new_node;
+}
+
+/**
+ * Returns the first snode with the given string.
+ *
+ * @param l pointer to the list (non-NULL)
+ * @parap str pointer to a string
+ * @return struct snode* or NULL if no match
+ */
+
+struct snode* slist_find(struct slist *l, char *str)
+{
+    struct snode *current = l->front;
+    while (current) {
+        if (strcmp(current->str, str) == 0) {
+            return current;
+        }
+        current = current -> next;
+    }
+    return NULL;
+}
+/**
+ * Deallocate a list and all snodes
+ *
+ * @param l pointer tot he list
+ */
+void slist_destroy(struct slist *l)
+{
+    struct snode *current = l->front;
+    while (current) {
+        struct snode *tmp = current;
+        current = current -> next;
+        snode_destroy(tmp);
+    }
+
+}
+
+/**
+ * Traverse the list and print the content of each node.
+ *
+ * @param l pointer to the list (non-NULL)
+ */
+void slist_traverse(struct slist *l)
+{
+    struct snode *current = l->front;
+    while (current) {
+        printf("%s ", current->str);
+        current = current -> next;
+    }
+    printf("\n");
+}
+
+/**
+ * Returns the number of elements in the list (nodes).
+ *
+ * @param l pointer to the list (non-NULL)
+ */
+uint32_t slist_length(struct slist *l)
+{
+    return (uint32_t) (l->size);
+}
+/**
+ * Deletes the first snode with the given string.
+ *
+ * @param l pointer to the list (non-NULL)
+ * @parap str pointer to a string
+ * @return struct snode* or NULL if no match
+ */
+struct snode* slist_delete(struct slist *l, char *str)
+{
+    struct snode *current = l->front;
+    while (current->next) {
+        if(strcmp(current->next->str, str) == 0) { // Found
+            struct snode *tmp = current->next;
+            current->next = current->next->next;
+            tmp->next = NULL;
+            l->size = l->size - 1;
+            return tmp;
+        }
+        current = current -> next;
+    }
+    return NULL;
+}
+
+struct snode * slist_get_front(struct slist *l)
+{
+    return l->front;
+}
+
+struct snode * slist_get_back(struct slist *l)
+{
+    return l->back;
+}
---
+++ lab6/task1/snode.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct snode {
+  //TODO: change str to dynamic allcoation
+  char *str;
+  struct snode *next;
+};
+
+//TODO: change prototypes of functions to remove
+//length
+struct snode *snode_create(char *s) ;
+void snode_destroy(struct snode * s) ;
