d6d3fae9fd346ea962187dc5031b4fb7a70d1908
Peiying Yang <py30732n@pace.edu>
2020-03-27 18:45:00 -0400

    task3

--- lab6/task3/slist.h
+++ lab6/task3/slist.h
@@ -39,6 +39,7 @@
 struct slist {
   struct snode *front; // front node
   struct snode *back;  // back node
+  int size;
 };
 
 /**
@@ -106,6 +107,8 @@ struct snode* slist_delete(struct slist *l, char *str);
 
 struct snode * slist_get_front(struct slist *l);
 struct snode * slist_get_back(struct slist *l);
+struct snode * slist_find_at(struct slist *l, int index);
+void slist_delete_at(struct slist *l, int index);
 
 
 #endif /* _slist_H_ */
