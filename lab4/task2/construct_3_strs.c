218a1eb742fc7a79a16a87ef6db1a1c9f9b7c94c
Peiying Yang <py30732n@pace.edu>
2020-05-03 22:40:07 -0400

    task2

--- lab4/task2/construct_3_strs.c
+++ lab4/task2/construct_3_strs.c
@@ -1,12 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 typedef struct node {
-    char * value; //must use dynamic allocation 
+    char * value; //must use dynamic allocation
     struct node* next;
 } node_t;
 
 node_t * construct_3_strs() {
-    return NULL;//just to pass compiler, please edit as needed.
+    struct node* x;
+    struct node* y;
+    struct node* z;
+    //Allocate three Node pointees and store references to them in the three pointers.
+    x = malloc(sizeof(struct node));
+    y = malloc(sizeof(struct node));
+    z = malloc(sizeof(struct node));
+    //Dereference each pointer to store the appropriate number into the value field in its pointee.
+    x->value = malloc(sizeof(char) * (strlen("CS232")+1));
+    strcpy(x->value, "CS232");
+    y->value = malloc(sizeof(char) * (strlen("is")+1));
+    strcpy(y->value, "is");
+    z->value = malloc(sizeof(char) * (strlen("awesome")+1));
+    strcpy(z->value, "awesome");
+    //Dereference each pointer to access the .next field in its pointee,
+    //and use pointer assignment to set the .next field to point to the appropriate Node.
+    x->next = y;
+    y->next = z;
+    z->next = x;
+    return x;
 }
 
 //You can ignore the following code for testing
