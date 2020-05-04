1cf14a63cee65ee53f8dbac7ff30b3650ba3b1fc
Peiying Yang <py30732n@pace.edu>
2020-05-04 12:39:14 -0400

    task2

--- lab5/task2/construct_3_structs_add.c
+++ lab5/task2/construct_3_structs_add.c
@@ -5,23 +5,49 @@ typedef struct snode node_t;
 
 node_t * setup() {
     //TODO:copy setup func from task1
+    node_t * head;
+    node_t * node1 = (node_t*)malloc(sizeof(node_t));
+    node_t * node2 = (node_t*)malloc(sizeof(node_t));
+    node_t * node3 = (node_t*)malloc(sizeof(node_t));
+    head = node1;
+    strcpy(node1->str, "hello");
+    node1->length = 5;
+    strcpy(node2->str, "there");
+    node2->length =5;
+    strcpy(node3->str, "prof");
+    node3->length = 4;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = NULL;
+    return head;
 }
 
-void teardown() {
+void teardown(node_t * head) {
     //TODO: free all dynamic memory you requested.
+    while(head!=NULL) {
+        node_t *temp = head;
+        head = head -> next;
+        temp->next = NULL;
+        free(temp);
+    }
 }
 
-void add(node_t * head, char * str, int length){
+void add(node_t **head, char * str, int length){
     //TODO: implement add to add a new node to front, pointed by head
+    node_t * node = (node_t*)malloc(sizeof(node_t));
+    strcpy(node->str, str);
+    node->length = length;
+    node->next = *head;
+    *head = node;
 }
 
 //You can ignore the following code for testing
 void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
-    add(head, "hi", 2);
+    add(&head, "hi", 2);
     dump_all(head);
-    teardown();
+    teardown(head);
     return 0;
 }
 
