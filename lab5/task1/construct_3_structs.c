699136705566237d3ac863161e47dc1211ef6c1b
Peiying Yang <py30732n@pace.edu>
2020-05-04 12:37:01 -0400

    task1

--- lab5/task1/construct_3_structs.c
+++ lab5/task1/construct_3_structs.c
@@ -4,19 +4,43 @@
 typedef struct snode node_t;
 
 node_t * setup() {
-    node_t * head ;//head declared for you
+    node_t * head;
+    node_t * node1 = (node_t*)malloc(sizeof(node_t));
+    node_t * node2 = (node_t*)malloc(sizeof(node_t));
+    node_t * node3 = (node_t*)malloc(sizeof(node_t));
+    head = node1;
+    strcpy(node1->str, "hello");
+    node1->length = 5;
+    strcpy(node2->str, "there");
+    node2->length =5;
+    strcpy(node3->str, "prof");
+    node3->length = 4;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = NULL;
+
+
+    //TODO:head declared for you
     //Allocate three more pointers
     //head for the first Node, and temporary pointers node1, node2 and node3
     //Allocate three node pointees and store references to them in the three pointers
-    //Dereference each pointer to store the appropriate number into the value field in its pointee.
-    //Dereference each pointer to access the .next field in its pointee, 
-    //and use pointer assignment to set the .next field to point to the appropriate Node. 
-    
+    //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
+    //Dereference each pointer to access the .next field in its pointee,
+    //and use pointer assignment to set the .next field to point to the appropriate Node.
+
    return head;
 }
 
-void teardown() {
+void teardown(node_t * head) {
     //TODO: free all dynamic memory you requested.
+    //Please complete the prototype of teardown.
+    //You are not allowed to use globals
+    while(head!=NULL) {
+        node_t *temp = head;
+        head = head -> next;
+        temp->next = NULL;
+        free(temp);
+    }
 }
 
 //You can ignore the following code for testing
@@ -24,7 +48,7 @@ void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
     dump_all(head);
-    teardown();
+    teardown(head);
     return 0;
 }
 
